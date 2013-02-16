50deb0186849144dfe3fd2d909a2f61e0179310e
Pedro Maciel <pedrozath@gmail.com>
2013-02-16 12:31:14 -0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ CiaDasCapas::Application.routes.draw do
 	ActiveAdmin.routes(self)
 	devise_for :admin_users, ActiveAdmin::Devise.config
 
+	get "/webmail" => redirect("http://www.google.com/a/capasparasofa.com.br")
 	match "/quem-somos" => "pages#index"
 	match "/contato" => "contatos#new"
 	match "/envio" => "contatos#create"
